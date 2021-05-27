6af010448a8624fe6ecedac6d9cd8860cc56ff7c
KrutikaBhatt <krutikabhatt222@gmail.com>
2021-05-27 19:47:20 +0530

    Added status code test

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
+class HomeTest(TestCase):
+    def test_homepage_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
