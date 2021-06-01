f69699db7c13c3bce70c4383f255c6c9f0469080
KrutikaBhatt <krutikabhatt222@gmail.com>
2021-06-01 18:00:13 +0530

    Fixed slidebar issue

--- static/js/slidebar.js
+++ static/js/slidebar.js
@@ -2,7 +2,13 @@
 $(document).ready(function () {
 
     $('#sidebarCollapse').on('click', function () {
+
         $('#sidebar').toggleClass('active');
+        const elem = document.getElementById('Toggle-button');
+        console.log(elem.className);
+        elem.className="fa fa-times";
+        console.log(elem.className);
     });
+
 
 });
