2cb20b9724a12fb1a023a057d398db3676df6c79
KrutikaBhatt <krutikabhatt222@gmail.com>
2021-06-01 20:34:23 +0530

    Dynamic navbar

--- static/js/slidebar.js
+++ static/js/slidebar.js
@@ -1,14 +1,16 @@
 
+// $(document).ready(function () {
+
+//     $('#sidebarCollapse').on('click', function () {
+
+//         $('#sidebar').toggleClass('active');
+//     });
+
+
+// });
 $(document).ready(function () {
-
     $('#sidebarCollapse').on('click', function () {
-
         $('#sidebar').toggleClass('active');
-        const elem = document.getElementById('Toggle-button');
-        console.log(elem.className);
-        elem.className="fa fa-times";
-        console.log(elem.className);
+        $(this).toggleClass('active');
     });
-
-
 });
