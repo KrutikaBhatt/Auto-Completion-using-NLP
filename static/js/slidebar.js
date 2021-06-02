d8fd7388c4100f7a5f5bca6f274d81b46e31c9ee
KrutikaBhatt <krutikabhatt222@gmail.com>
2021-06-02 12:24:07 +0530

    Added the text fields

--- static/js/slidebar.js
+++ static/js/slidebar.js
@@ -1,13 +1,4 @@
 
-// $(document).ready(function () {
-
-//     $('#sidebarCollapse').on('click', function () {
-
-//         $('#sidebar').toggleClass('active');
-//     });
-
-
-// });
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
