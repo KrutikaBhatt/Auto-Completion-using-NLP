2935c6446018053b1f557f5d2a39fd92855b3e8f
KrutikaBhatt <krutikabhatt222@gmail.com>
2021-06-03 12:14:02 +0530

    Form inputs done

--- static/js/slidebar.js
+++ static/js/slidebar.js
@@ -2,6 +2,7 @@
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
+        $('#form_custom_class').toggleClass('active');
         $(this).toggleClass('active');
     });
 });
